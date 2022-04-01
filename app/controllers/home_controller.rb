617ff41b96cf6786db7397ab627955ce53dd3340
Samuel Barros <samuel@swind.com.br>
2022-03-31 23:59:28 -0300

    Mais ajustes de layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @month_sales = Sale.select("distinct (concat(extract(year from sale_date),'-',extract(month from sale_date))) year_month")
+    @month_sales = Sale.select("distinct (concat(extract(year from sale_date),'-',lpad(extract(month from sale_date)::text, 2, '0'))) year_month")
 
     if params['year_month'].nil?
       ## Default
-      best_client_month(Time.zone.now.year.to_s + '-' + Time.zone.now.month.to_s)
+      best_client_month(Time.zone.now.try(:strftime, '%Y-%m'))
     else
-       @month_sales = Sale.select("distinct (concat(extract(year from sale_date),'-',extract(month from sale_date))) year_month")
+       @month_sales = Sale.select("distinct (concat(extract(year from sale_date),'-',lpad(extract(month from sale_date)::text, 2, '0'))) year_month")
       best_client_month(params['year_month'])
     end
   end
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
     stSQL = ''
     stSQL << 'select * '
     stSQL << ' from ( '
-    stSQL << "   select distinct (concat(extract(year from sale_date),'-',extract(month from sale_date))) ano_mes, * from sales "
+    stSQL << "   select distinct (concat(extract(year from sale_date),'-',lpad(extract(month from sale_date)::text, 2, '0'))) ano_mes, * from sales "
     stSQL << " ) sales where ano_mes = '#{year_month}' order by total_price desc limit(3);"
 
     @clients = ActiveRecord::Base.connection.execute(stSQL)
